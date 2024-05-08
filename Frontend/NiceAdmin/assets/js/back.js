a712f0aa30fe3dbfb0fddbdffb977f1592ab0f7b
Zorro1975 <mmenacho1975@gmail.com>
2024-05-08 12:21:47 -0500

    cambios_20240508_12_20
    Se actualizo el CorsConfig para que no de error al jalar la data en los html

--- Frontend/NiceAdmin/assets/js/back.js
+++ Frontend/NiceAdmin/assets/js/back.js
@@ -1,13 +1,13 @@
 $.ajax({
-  url: "localhost:8080/api/area/all"
-})
-  .done(function( data ) {
-      console.log( "Sample of data:", data);
-	  var area = data;
-	  let cad = '';
-	  area.forEach(function(area, index) {
-		html+='<option value="'+area.ard_id+'">'+area.ard_descri+'</option>'
-	});
-
-	$("#ard_id").html(cad);
+  url: "http://localhost:8080/api/areaDenominacion/all"
+  })
+	.done(function( data ) {		
+		var area = data;		
+		let html = '<option selected>-- Seleccione --</option>';
+		area.forEach(function(area, index) {
+			console.log(area);
+			html+='<option value="'+area.id+'">'+area.descripcion+'</option>'
+		});
+	$("#ard_id").html(html);
 });
+
--- src/main/java/empresaTransporte/utp/config/CorsConfig.java
+++ src/main/java/empresaTransporte/utp/config/CorsConfig.java
@@ -11,16 +11,14 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
 
-
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
-
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*");
+        }
+    };
 
     }
 }
