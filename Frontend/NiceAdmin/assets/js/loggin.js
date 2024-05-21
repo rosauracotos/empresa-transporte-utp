106f938cb19753263a822e3ca58c13e0e785219d
Zorro1975 <mmenacho1975@gmail.com>
2024-05-21 10:03:34 -0500

    cambios_20240521_1002
    Se quito carpeta porque esos el proyecto se movio a otra carpeta

--- Frontend/NiceAdmin/assets/js/loggin.js
+++
@@ -1,8 +0,0 @@
-let boton = document.getElementById("Ingresar");
-console.log(boton);
-// Agregar un event listener para el evento de clic
-boton.addEventListener("click", function () {
-  // Este código se ejecutará cuando el botón sea clicado
-  //window.location.href = "http://127.0.0.1:5500/Frontend/NiceAdmin/inicio.html";
-  window.location.href = "inicio.html";
-});
